98b4a296729a6d57b58f2889dea32105e41f2225
Kripakaran <itskripakaran@gmail.com>
2021-05-27 11:19:53 -0700

    feat: Start new angular form and emit to parent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './auth-form/User';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advanced-angular';
+
+  handleSubmit(emittedUser: User){
+    console.log(emittedUser);
+  }
 }
---
+++ src/app/auth-form/auth-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { User } from './User';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.css']
+})
+
+
+
+export class AuthFormComponent implements OnInit {
+
+  @Output()
+  submittedUser: EventEmitter<User> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(user: User){
+    // console.log(user);
+    this.submittedUser.emit(user);
+  }
+
+}
